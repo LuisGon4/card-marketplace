54fd514623eb1e4f928d27328aec6b34a4901e6b
Luis Gonzalez <luismgon4@gmail.com>
2026-08-05 22:06:27 -0700

    fix: bucket cors config fixed

--- localstack/init-s3.sh
+++ localstack/init-s3.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 awslocal s3 mb s3://${AWS_S3_BUCKET}
+
+awslocal s3api put-bucket-cors --bucket ${AWS_S3_BUCKET} --cors-configuration '{
+  "CORSRules": [
+    {
+      "AllowedOrigins": ["http://localhost:5173"],
+      "AllowedMethods": ["PUT", "GET"],
+      "AllowedHeaders": ["*"],
+      "ExposeHeaders": ["ETag"],
+      "MaxAgeSeconds": 3000
+    }
+  ]
+}'
--- src/main/java/com/spring/cardmarketplace/configuration/CorsConfig.java
+++ src/main/java/com/spring/cardmarketplace/configuration/CorsConfig.java
@@ -2,7 +2,6 @@ package com.spring.cardmarketplace.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
