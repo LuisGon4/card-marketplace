af33638a95702bdd16d4c6ef16cf8d426c15293b
Luis Gonzalez <luismgon4@gmail.com>
2026-08-13 16:44:35 -0700

    fix: dev properties records created

--- src/main/java/com/spring/cardmarketplace/configuration/S3Config.java
+++ src/main/java/com/spring/cardmarketplace/configuration/S3Config.java
@@ -13,17 +13,17 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 import java.net.URI;
 
-@EnableConfigurationProperties(S3Properties.class)
+@EnableConfigurationProperties({S3Properties.class, DevS3Properties.class})
 @Configuration
 public class S3Config {
     @Profile("dev")
     @Bean
-    public S3Client devS3Client(S3Properties props) {
+    public S3Client devS3Client(S3Properties props, DevS3Properties devProps) {
         return S3Client.builder()
                 .region(Region.of(props.region()))
-                .endpointOverride(URI.create(props.endpoint()))
+                .endpointOverride(URI.create(devProps.endpoint()))
                 .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(props.accessKey(), props.secretKey())))
+                        AwsBasicCredentials.create(devProps.accessKey(), devProps.secretKey())))
                 .forcePathStyle(true)
                 .build();
     }
@@ -47,12 +47,12 @@ public class S3Config {
 
     @Profile("dev")
     @Bean
-    public S3Presigner devS3Presigner(S3Properties props) {
+    public S3Presigner devS3Presigner(S3Properties props, DevS3Properties devProps) {
         return S3Presigner.builder()
                 .region(Region.of(props.region()))
-                .endpointOverride(URI.create(props.endpoint()))
+                .endpointOverride(URI.create(devProps.endpoint()))
                 .credentialsProvider(StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(props.accessKey(), props.secretKey())))
+                                AwsBasicCredentials.create(devProps.accessKey(), devProps.secretKey())))
                 .serviceConfiguration(S3Configuration.builder()
                         .pathStyleAccessEnabled(true)
                         .build())
